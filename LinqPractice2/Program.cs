75d98245a09614622f30d885700272d999e4b8fb
Lake Phillips <lakephillips3232@gmail.com>
2024-05-11 12:19:46 -0500

    check

---
+++ LinqPractice2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinqPractice2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] mainNums = { "robert" , "brooke", "trent", "Becky" , "adam"};
+
+            var people = mainNums.Where(p => p.Length == 5 && p.Contains("e"));
+            foreach(var p in people)
+            {
+                Console.WriteLine(p);
+            }
+
+        }
+    }
+}
